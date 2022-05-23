2e38ce2910724cf5d85c5631ab835de5f26e8881
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-23 18:00:59 +0200

    model quasi fini

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -3,4 +3,9 @@ class Toilet < ApplicationRecord
   has_many :bookings
   has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :genders
+
+  validates :type, presence: true, inclusion: { :in => ['urinal', 'washing toilet', 'Turkish toilet', 'dry toilet', 'chemical toilet'] }
+  validates :price, presence: true
+  validates :location, presence: true
+
 end
