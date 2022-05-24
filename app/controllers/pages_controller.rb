1f3448ce8063b44d0e1cc01da8af09675e7f84f8
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-24 12:28:44 +0200

    add dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
+    @toilets = Toilet.all
+  end
+
+  def dashboard
+    @user = current_user
+    @reviews = @user.reviews
+    @bookings = @user.bookings
+    @toilets = @user.toilets
+    @new_booking = Booking.new
   end
 end
