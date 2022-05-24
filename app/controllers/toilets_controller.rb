d9f636984d45e7e547f876cada748b33933ebd4a
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-24 12:31:42 +0200

    add a description to toilet

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -38,7 +38,7 @@ class ToiletsController < ApplicationController
 
   private
   def toilet_params
-    params.require(:toilet).permit(:location, :type, :price, :name)
+    params.require(:toilet).permit(:location, :type, :price, :name, :description)
   end
 
   def find_toilet
