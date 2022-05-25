7ad987966bae24a38102dbed0253325a2b5aa2f9
Archibald Saen <archibaldsaen98@gmail.com>
2022-05-25 11:14:47 +0200

    saving

--- db/migrate/20220523152026_create_toilet_genders.rb
+++
@@ -1,8 +0,0 @@
-class CreateGendersToilets < ActiveRecord::Migration[6.1]
-  def change
-    create_table :genders_toilets do |t|
-      t.references :gender, null: false, foreign_key: true
-      t.references :toilet, null: false, foreign_key: true
-    end
-  end
-end
