83ba940f6b18e1432773ca28ce0375363336d0ad
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-26 12:36:52 +0200

    added descending order for toilet show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    @toilets = Toilet.all
+    @toilets = Toilet.all.order("created_at desc")
   end
 
   def dashboard
