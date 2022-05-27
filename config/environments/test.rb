318c51007f559ae6788695914b00f6545e748526
sebastien dellis <sebastien.dellis@gmail.com>
2022-05-27 14:49:40 +0200

    heroku fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-
+  config.serve_static_assets = true
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
